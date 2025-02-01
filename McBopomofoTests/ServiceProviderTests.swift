c45c484b0e586ed8f1b26d3b750001c8aee48633
zonble <zonble@gmail.com>
2025-02-02 00:59:20 +0800

    Coverts to Swift tests.

--- McBopomofoTests/ServiceProviderTests.swift
+++ McBopomofoTests/ServiceProviderTests.swift
@@ -21,194 +21,122 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-import XCTest
+//import XCTest
+import Testing
 
 @testable import McBopomofo
 
-final class ServiceProviderTests: XCTestCase {
-    func testExtractReading0() {
+@Suite("Test the service provider")
+final class ServiceProviderTests {
+    @Test(
+        "Test extrct reading",
+        arguments: [
+            ("", ""),
+            ("電腦", "ㄉㄧㄢˋ-ㄋㄠˇ"),
+            ("🔥🔥🔥", "ㄏㄨㄛˇ-ㄏㄨㄛˇ-ㄏㄨㄛˇ"),
+            ("🔥", "ㄏㄨㄛˇ"),
+            (" ", "？"),
+            ("！", "_ctrl_punctuation_!"),
+
+        ])
+    func testExtractReading(input: String, expected: String) {
         let provider = ServiceProvider()
-        let output = provider.extractReading(from: "")
-        XCTAssert(output == "", output)
+        let output = provider.extractReading(from: input)
+        #expect(output == expected)
     }
 
-    func testExtractReading1() {
-        let provider = ServiceProvider()
-        let output = provider.extractReading(from: "電腦")
-        XCTAssert(output == "ㄉㄧㄢˋ-ㄋㄠˇ")
-    }
-
-    func testExtractReading2() {
-        let provider = ServiceProvider()
-        let output = provider.extractReading(from: "🔥🔥🔥")
-        XCTAssert(output == "ㄏㄨㄛˇ-ㄏㄨㄛˇ-ㄏㄨㄛˇ")
-    }
-
-    func testExtractReading3() {
-        let provider = ServiceProvider()
-        let output = provider.extractReading(from: "🔥")
-        XCTAssert(output == "ㄏㄨㄛˇ")
-    }
-
-    func testExtractReading4() {
-        let provider = ServiceProvider()
-        let output = provider.extractReading(from: " ")
-        XCTAssert(output == "？", output)
-    }
-
-    func testExtractReading5() {
-        let provider = ServiceProvider()
-        let output = provider.extractReading(from: "！")
-        XCTAssert(output == "_ctrl_punctuation_!", output)
-    }
-
-    func testAddReading() {
+    @Test(
+        "Test add readings",
+        arguments: [
+            ("美好的朝陽", "美(ㄇㄟˇ)好(ㄏㄠˇ)的(ㄉㄜ˙)朝(ㄓㄠ)陽(ㄧㄤˊ)"),
+            (
+                "『這樣可以嗎？』我相當好奇的問了他，但是還是不知道他的意思",
+                "『這(ㄓㄜˋ)樣(ㄧㄤˋ)可(ㄎㄜˇ)以(ㄧˇ)嗎(ㄇㄚ)？』我(ㄨㄛˇ)相(ㄒㄧㄤ)當(ㄉㄤ)好(ㄏㄠˋ)奇(ㄑㄧˊ)的(ㄉㄜ˙)問(ㄨㄣˋ)了(ㄌㄜ˙)他(ㄊㄚ)，但(ㄉㄢˋ)是(ㄕˋ)還(ㄏㄞˊ)是(ㄕˋ)不(ㄅㄨˊ)知(ㄓ)道(ㄉㄠˋ)他(ㄊㄚ)的(ㄉㄜ˙)意(ㄧˋ)思(ㄙ)"
+            ),
+        ])
+    func testAddReading(input: String, expected: String) {
         LanguageModelManager.loadDataModels()
         let provider = ServiceProvider()
-        let input = "美好的朝陽"
-        let expected = "美(ㄇㄟˇ)好(ㄏㄠˇ)的(ㄉㄜ˙)朝(ㄓㄠ)陽(ㄧㄤˊ)"
         let output = provider.addReading(string: input)
-        XCTAssert(output == expected, output)
+        #expect(output == expected)
     }
 
-    func testTokenize1() {
+    @Test(
+        "Test tokenize",
+        arguments: [
+            ("『『』『』『", ["『", "『", "』", "『", "』", "『"]),
+            ("『這樣可以嗎？』", ["『", "這樣", "可以", "嗎", "？", "』"]),
+        ])
+    func testTokenize1(input: String, expected: [String]) {
         let provider = ServiceProvider()
-        let output = provider.tokenize(string: "『『』『』『")
-        let expected = ["『", "『", "』", "『", "』", "『"]
-        XCTAssert(output.map { $0.0 } == expected, "\(output)")
+        let output = provider.tokenize(string: input)
+        #expect(output.map { $0.0 } == expected, "\(output)")
     }
 
-    func testTokenize2() {
-        let provider = ServiceProvider()
-        let output = provider.tokenize(string: "『這樣可以嗎？』")
-        let expected = ["『", "這樣", "可以", "嗎", "？", "』"]
-        XCTAssert(output.map { $0.0 } == expected, "\(output)")
-    }
-
-    func testAddReading2() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let input = "『這樣可以嗎？』我相當好奇的問了他，但是還是不知道他的意思"
-        let expected =
-            "『這(ㄓㄜˋ)樣(ㄧㄤˋ)可(ㄎㄜˇ)以(ㄧˇ)嗎(ㄇㄚ)？』我(ㄨㄛˇ)相(ㄒㄧㄤ)當(ㄉㄤ)好(ㄏㄠˋ)奇(ㄑㄧˊ)的(ㄉㄜ˙)問(ㄨㄣˋ)了(ㄌㄜ˙)他(ㄊㄚ)，但(ㄉㄢˋ)是(ㄕˋ)還(ㄏㄞˊ)是(ㄕˋ)不(ㄅㄨˊ)知(ㄓ)道(ㄉㄠˋ)他(ㄊㄚ)的(ㄉㄜ˙)意(ㄧˋ)思(ㄙ)"
-        let output = provider.addReading(string: input)
-        XCTAssert(output == expected, output)
-    }
-
-    func testConvertBrailleToChinese1() {
+    @Test(
+        "Test converting Taiwanese Braille to Chinese",
+        arguments: [
+            ("⠰⠤⠋⠺⠂⠻⠄⠛⠥⠂⠓⠫⠐⠑⠳⠄⠪⠐⠙⠮⠁⠅⠎⠐⠊⠱⠐⠑⠪⠄⠏⠣⠄⠇⠶⠐⠤⠆", "「台灣人最需要的就是消波塊」")
+        ])
+    func testConvertBrailleToChinese(input: String, expected: String) {
         LanguageModelManager.loadDataModels()
         let provider = ServiceProvider()
         let helper = ServiceProviderInputHelper()
         provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "⠰⠤⠋⠺⠂⠻⠄⠛⠥⠂⠓⠫⠐⠑⠳⠄⠪⠐⠙⠮⠁⠅⠎⠐⠊⠱⠐⠑⠪⠄⠏⠣⠄⠇⠶⠐⠤⠆"
-        let expected = "「台灣人最需要的就是消波塊」"
         let output = provider.convertBrailleToChineseText(string: input)
-        XCTAssert(output == expected, output)
+        #expect(output == expected, "\(output)")
     }
 
-    func testConvertBrailleToChinese2() {
+    @Test(
+        "Test coverting Chinese to Taiwanese Braille, then coverting it back",
+        arguments: [
+            ("由「小麥」的作者", ""),
+            ("This is a test", ""),
+            ("第1名", "地 1 明"),
+            ("第A名", "地 A 明"),
+            ("第AB名", "地 AB 明"),
+            ("第A1名", "地 A1 明"),
+        ])
+    func testConvertBrailleThenBack(input: String, expected: String) {
         LanguageModelManager.loadDataModels()
         let provider = ServiceProvider()
         let helper = ServiceProviderInputHelper()
         provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "⠰⠤⠋⠺⠂⠻⠄⠛⠥⠂⠓⠫⠐⠑⠳⠄⠪⠐⠙⠮⠁⠅⠎⠐⠊⠱⠐⠑⠪⠄⠏⠣⠄⠇⠶⠐⠤⠆"
-        let expected = "「台灣人最需要的就是消波塊」"
-        let output = provider.convertBrailleToChineseText(string: input)
-        XCTAssert(output == expected, output)
-    }
-
-    func testConvertBrailleThenBack1() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "由「小麥」的作者"
-        let r1 = provider.convertToBraille(string: input)
-        print(r1)
-        let r2 = provider.convertBrailleToChineseText(string: r1)
-        XCTAssert(r2 == input, r2)
-    }
-
-    func testConvertBrailleThenBack2() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "This is a test"
         let r1 = provider.convertToBraille(string: input)
         let r2 = provider.convertBrailleToChineseText(string: r1)
-        XCTAssert(r2 == input, r2)
+        if expected == "" {
+            #expect(r2 == input, "\(r2)")
+        } else {
+            #expect(r2 == expected, "\(r2)")
+        }
     }
 
-    func testConvertBrailleThenBack3() {
+    @Test(
+        "Test coverting letters to Taiwanese Braille",
+        arguments: [
+            ("This is a test", "⠠⠞⠓⠊⠎ ⠊⠎ ⠁ ⠞⠑⠎⠞"),
+            ("This is a test 台灣人最需要的就是消波塊", "⠠⠞⠓⠊⠎ ⠊⠎ ⠁ ⠞⠑⠎⠞ ⠋⠺⠂⠻⠄⠛⠥⠂⠓⠫⠐⠑⠳⠄⠪⠐⠙⠮⠁⠅⠎⠐⠊⠱⠐⠑⠪⠄⠏⠣⠄⠇⠶⠐"),
+        ])
+    func testLetters(input: String, expected: String) {
         LanguageModelManager.loadDataModels()
         let provider = ServiceProvider()
         let helper = ServiceProviderInputHelper()
         provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "第1名"
-        let r1 = provider.convertToBraille(string: input)
-        XCTAssert(r1 == "⠙⠡⠐ ⠼⠂ ⠍⠽⠂", r1)
-        let r2 = provider.convertBrailleToChineseText(string: r1)
-        XCTAssert(r2 == "地 1 明", r2)
+        let result = provider.convertToBraille(string: input)
+        #expect(result == expected)
     }
 
-    func testConvertBrailleThenBack4() {
+    @Test(
+        "Test coverting digits to Taiwanese Braille",
+        arguments: [
+            ("24", "⠼⠆⠲")
+        ])
+    func testDigit(input: String, expected: String) {
         LanguageModelManager.loadDataModels()
         let provider = ServiceProvider()
         let helper = ServiceProviderInputHelper()
         provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "第A名"
-        let r1 = provider.convertToBraille(string: input)
-        let r2 = provider.convertBrailleToChineseText(string: r1)
-        XCTAssert(r2 == "地 A 明", r2)
-    }
-
-    func testConvertBrailleThenBack5() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "第AB名"
-        let r1 = provider.convertToBraille(string: input)
-        let r2 = provider.convertBrailleToChineseText(string: r1)
-        XCTAssert(r2 == "地 AB 明", r2)
-    }
-
-    func testConvertBrailleThenBack6() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let input = "第A1名"
-        let r1 = provider.convertToBraille(string: input)
-        let r2 = provider.convertBrailleToChineseText(string: r1)
-        XCTAssert(r2 == "地 A1 明", r2)
-    }
-
-    func testLetters1() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let result = provider.convertToBraille(string: "This is a test")
-        XCTAssert(result == "⠠⠞⠓⠊⠎ ⠊⠎ ⠁ ⠞⠑⠎⠞")
-    }
-
-    func testLetters2() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let result = provider.convertToBraille(string: "This is a test 台灣人最需要的就是消波塊")
-        XCTAssert(result == "⠠⠞⠓⠊⠎ ⠊⠎ ⠁ ⠞⠑⠎⠞ ⠋⠺⠂⠻⠄⠛⠥⠂⠓⠫⠐⠑⠳⠄⠪⠐⠙⠮⠁⠅⠎⠐⠊⠱⠐⠑⠪⠄⠏⠣⠄⠇⠶⠐", result)
-    }
-
-    func testDigit() {
-        LanguageModelManager.loadDataModels()
-        let provider = ServiceProvider()
-        let helper = ServiceProviderInputHelper()
-        provider.delegate = helper as? any ServiceProviderDelegate
-        let result = provider.convertToBraille(string: "24")
-        XCTAssert(result == "⠼⠆⠲", result)
+        let result = provider.convertToBraille(string: input)
+        #expect(result == expected)
     }
 }
